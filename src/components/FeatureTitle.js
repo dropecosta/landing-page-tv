d4a4535933eefaab9971e8d7d2c2fbcfa3abbf67
dropecosta <pedro-reis@puc.br>
2019-05-10 18:16:13 -0300

    adding styles to FeatureTitle

--- src/components/FeatureTitle.js
+++ src/components/FeatureTitle.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import logoBbb from '../img/logo_bbb.png';
+import logobbb from '../img/logo_bbb.png';
+
+import styled from 'styled-components'
+
+const Main = styled.div`
+    .logo{
+        height: 8rem;
+        width: auto;
+        position: relative;
+        z-index: 3;
+    }
+
+    h1{
+        color: white;
+        font-weight: 500;
+        margin-bottom: 2.5rem;
+        white-space: nowrap;
+        position: relative;
+        z-index: 2;
+      }
+`
+
 
 class FeatureTitle extends React.Component {
-    render(){
+    render() {
         return(
-            <div>
-                <img className="logo" src={logoBbb} alt="Logo Big Brother Brasil"/>
+            <Main>
+                <img className="logo" src={logobbb} alt="Logo Big Brother Brasil"/>
                 <h1>Acompanhe 24h ao vivo a casa mais<br /> vigiada do Brasil</h1>
-            </div>
+            </Main>
         )
     }
 }
